3e7454807f94fe7a015af0c978d12e07e46971a1
Joao Costa <joaopedrocosta.contato@gmail.com>
2025-07-24 23:41:03 -0300

    Reset repo

--- apps/mfe-react/src/components/Title.tsx
+++
@@ -1,5 +0,0 @@
-export default function Title() {
-    return (
-        <h1>React microfrontend</h1>
-    );
-}
--- apps/mfes-workspace/app/routes.tsx
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index('./app.tsx'),
-  route('about', './routes/about.tsx'),
-  route('list', './routes/list.tsx'),
-  ] satisfies RouteConfig;
